350ba63c277e193f778b6af1b5b6c9220031fab3
IVARDAI-PC\ispy7 <vardai.istvan@selester.hu>
2021-02-19 19:55:55 +0100

    can download from blob

--- STREAM/StreamFiles/index.js
+++ STREAM/StreamFiles/index.js
@@ -1,5 +1,5 @@
 const npm_mssql = require('mssql');
-const { BlobServiceClient } = require("@azure/storage-blob");
+const { BlobServiceClient } = require('@azure/storage-blob');
 const npm_streamifier = require('streamifier');
 const npm_multipart = require('multipart-formdata');
 
@@ -23,7 +23,7 @@ const dbConfig = {
     }
 };
 
-let request_id = "#";
+let request_id = '#';
 
 function watConnect() {
     return new Promise((resolve, reject) => {
@@ -49,17 +49,30 @@ function watSpExecute(dbRequest, procedureName) {
         })
     })
 }
+async function streamToBuffer(readableStream) {
+    return new Promise((resolve, reject) => {
+      const chunks = [];
+      readableStream.on('data', (data) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+      readableStream.on('end', () => {
+        resolve(Buffer.concat(chunks));
+      });
+      readableStream.on('error', reject);
+    });
+}
+
 resultErr = (errcode, err) => {
     return {
         status: 400,
         body: {
-            "header": {
-                "version": version,
-                "request_id": request_id,
-                "result": errcode
+            'header': {
+                'version': version,
+                'request_id': request_id,
+                'result': errcode
             },
             'body': {
-                "err": JSON.stringify(err)
+                'err': JSON.stringify(err)
             }
         }
     }
@@ -69,10 +82,10 @@ resultOk = (result) => {
     return {
         status: 200,
         body: {
-            "header": {
-                "version": version,
-                "request_id": request_id,
-                "result": "OK"
+            'header': {
+                'version': version,
+                'request_id': request_id,
+                'result': 'OK'
             },
             'body': result
         },
@@ -88,7 +101,7 @@ module.exports = async function (context, req) {
     let onErrorErrCode = '';
     let connected = false;
 
-    let watFunction = "";
+    let watFunction = '';
     let inputType = req.headers['content-type']
     let contentType = '';
 
@@ -131,7 +144,7 @@ module.exports = async function (context, req) {
     let watRequest = JSON.parse(messageArray[0].field);
     
     try {
-        watFunction = (watRequest.function) ? watRequest.function : '';
+        watFunction = (watRequest.header.function) ? watRequest.header.function : '';
     } catch (error) {
         // nothing to do    
     }
@@ -145,6 +158,20 @@ module.exports = async function (context, req) {
         //Step 2: Execute WAT Function
         onErrorErrCode = 'GATEWAY_ERROR_SQL-Statement failure'
 
+        let blobServiceClient;
+        let containerClient;
+                
+        try {
+            blobServiceClient = BlobServiceClient.fromConnectionString(storageConfig.storageConnectionString);
+            containerClient = blobServiceClient.getContainerClient(watRequest.header.portalOwnerId);
+            await containerClient.create();
+    
+        } catch (err) {
+            if (err.code !== 'ContainerAlreadyExists'){
+                context.res = resultErr('BLOB_ERROR - Create container failure', err)
+            }
+        }
+
         switch (watFunction) {
             //-------------------------------------------------------------------------------------------------------------------------------
             // WAT_INTERFACE_UPLOAD_FILE
@@ -155,27 +182,14 @@ module.exports = async function (context, req) {
                     break;
                 }
 
-                let folderName = (new Date()).toISOString().slice(0,10).replace(/-/g,"")
-                
-                let blobServiceClient;
-                let containerClient;
-                try {
-                    blobServiceClient = BlobServiceClient.fromConnectionString(storageConfig.storageConnectionString);
-                    containerClient = blobServiceClient.getContainerClient(watRequest.portalOwnerId);
-                    await containerClient.create();
-            
-                } catch (err) {
-                    if (err.code !== 'ContainerAlreadyExists'){
-                        context.res = resultErr('BLOB_ERROR - Create container failure', err)
-                    }
-                }
-
-                let fileName ='';
+                let filePath = (new Date()).toISOString().slice(0,10).replace(/-/g,'')
+                let fileName = '';
                 let outputFileIds = [];
+
                 for (let iFiles = 0; iFiles < filesArray.length; iFiles++) {
                     fileName = Math.random().toString(36);
 
-                    let blockBlobClient = containerClient.getBlockBlobClient(folderName.concat('/',fileName));
+                    let blockBlobClient = containerClient.getBlockBlobClient(filePath.concat('/', fileName));
     
                     let result;
                     try
@@ -189,20 +203,24 @@ module.exports = async function (context, req) {
     
                     dbRequest = new npm_mssql.Request(dbConn)
                     dbRequest.input('BLOB_Account', npm_mssql.NVarChar(50), blobServiceClient.accountName);
-                    dbRequest.input('BLOB_Container', npm_mssql.NVarChar(50), watRequest.portalOwnerId.toString());
-                    dbRequest.input('BLOB_Folder_Name', npm_mssql.NVarChar(50), folderName);
+                    dbRequest.input('BLOB_Container', npm_mssql.NVarChar(50), watRequest.header.portalOwnerId.toString());
+                    dbRequest.input('BLOB_File_Path', npm_mssql.NVarChar(255), filePath);
                     dbRequest.input('BLOB_File_Name', npm_mssql.NVarChar(255), fileName);
-                    dbRequest.input('BLOB_Orig_File_Name', npm_mssql.NVarChar(255), filesArray[iFiles].filename);
+                    dbRequest.input('BLOB_Orig_File_Name', npm_mssql.NVarChar(255), encodeURI(filesArray[iFiles].filename));
+                    dbRequest.input('BLOB_Content_Type', npm_mssql.NVarChar(255), filesArray[iFiles].type);
+                    dbRequest.input('BLOB_File_Length', npm_mssql.Int,filesArray[iFiles].data.length)
                     dbRequest.output('OUT_WAT_File_ID', npm_mssql.Int);
                     dbRequest.output('OUT_ErrCode', npm_mssql.NVarChar(255));
                     dbRequest.output('OUT_ErrParams', npm_mssql.NVarChar('max'));
+
                     dbResults = await watSpExecute(dbRequest, 'WAT_INTERFACE_UPLOAD_FILE');
+                    
                     if (dbResults.output.OUT_ErrCode != "") {
                         context.res = resultErr(dbResults.output.OUT_ErrCode, {
-                            "ReturnValues": {
-                                "ReturnValue": dbResults.returnValue,
-                                "ErrCode": dbResults.output.OUT_ErrCode,
-                                "ErrParams": dbResults.output.OUT_ErrParams
+                            'ReturnValues': {
+                                'ReturnValue': dbResults.returnValue,
+                                'ErrCode': dbResults.output.OUT_ErrCode,
+                                'ErrParams': dbResults.output.OUT_ErrParams
                             }
                         })
                     } else {
@@ -212,8 +230,65 @@ module.exports = async function (context, req) {
                 }
 
                 context.res = resultOk({
-                    "outputIds": JSON.stringify(outputFileIds)
+                    'outputIds': JSON.stringify(outputFileIds)
                 })
+
+                break;
+
+            //-------------------------------------------------------------------------------------------------------------------------------
+            // WAT_INTERFACE_DOWNLOAD_FILE
+            //-------------------------------------------------------------------------------------------------------------------------------
+            case 'WAT_INTERFACE_DOWNLOAD_FILE':
+                dbRequest = new npm_mssql.Request(dbConn)
+                dbRequest.input('WAT_Portal_Owners_ID', npm_mssql.Int, parseInt(watRequest.header.portalOwnerId));
+                dbRequest.input('WAT_Session_ID', npm_mssql.NVarChar(255), watRequest.header.watSessionId);
+                dbRequest.input('WAT_User', npm_mssql.NVarChar(128), watRequest.body.watUser);
+                dbRequest.input('BLOB_Account', npm_mssql.NVarChar(50), blobServiceClient.accountName);
+                dbRequest.input('File_ID', npm_mssql.Int, watRequest.body.fileId)
+                dbRequest.output('OUT_File_Meta_Data', npm_mssql.NVarChar('max'));
+                dbRequest.output('OUT_ErrCode', npm_mssql.NVarChar(255));
+                dbRequest.output('OUT_ErrParams', npm_mssql.NVarChar('max'));
+
+                dbResults = await watSpExecute(dbRequest, 'WAT_INTERFACE_DOWNLOAD_FILE');
+                
+                if (dbResults.output.OUT_ErrCode != "") {
+                    context.res = resultErr(dbResults.output.OUT_ErrCode, {
+                        'ReturnValues': {
+                            'ReturnValue': dbResults.returnValue,
+                            'ErrCode': dbResults.output.OUT_ErrCode,
+                            'ErrParams': dbResults.output.OUT_ErrParams
+                        }
+                    })
+                } else {
+                    fileMetaData = JSON.parse(dbResults.output.OUT_File_Meta_Data);
+                    if (fileMetaData.length == 0) {
+                        context.res = Result_OK({
+                            'Results': 'Nothing'
+                        })
+                    } else {
+                        let blobName = fileMetaData[0].filePath + '/' + fileMetaData[0].fileName;
+                        let blobClient = containerClient.getBlobClient(blobName);
+                        let downloadBlockBlobResponse = await blobClient.download();
+                        let result = (
+                          await streamToBuffer(downloadBlockBlobResponse.readableStreamBody)
+                        );
+    
+                        context.res = {
+                            status: 200,
+                            headers : {
+                                'Cache-Control': 'no-cache',
+                                'Content-Type': fileMetaData[0].contentType,
+                                'Content-Length': fileMetaData[0].fileLength,
+                                'Content-Disposition': 'attachment; filename=' + fileMetaData[0].origFileName,
+                                'Creation-Date': fileMetaData[0].creationDate
+                            },
+                            body : result
+                        }                    
+                    }
+                }
+
+
+
 
                 break;
 
