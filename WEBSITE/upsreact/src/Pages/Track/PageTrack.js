38578cfcf2e0789132700996059ae585eba8bddc
Laszlo Molnar <molnar.laszlo@selester.hu>
2021-06-21 16:00:43 +0200

    upc track & trace report development started

--- WEBSITE/upsreact/src/Pages/Track/PageTrack.js
+++ WEBSITE/upsreact/src/Pages/Track/PageTrack.js
@@ -1,77 +1,49 @@
-import "./PageTrack.scss"
-import { Redirect } from "react-router-dom"
-import HeaderLine from "../../Components/HeaderLine/HeaderLine"
-import * as Gl from "../../_SelComponents/_SelWebComponents/js/Gl"
-import TrackTraceSearchForm from "./subComponents/TrackTraceSearchForm/TrackTraceSearchForm"
-import langJSON from "./PageTrack-lang"
-import backgroundImage from "./PageTrack-background.png"
+import "./PageTrack.scss";
+import { Redirect } from "react-router-dom";
+import HeaderLine from "../../Components/HeaderLine/HeaderLine";
+import * as Gl from "../../_SelComponents/_SelWebComponents/js/Gl";
+import TevaTrckAndTraceFilter from "./subComponents/TevaTrackAndTraceFilter/TevaTrackAndTraceFilter";
+import langJSON from "./PageTrack-lang";
+import backgroundImage from "./PageTrack-background.png";
+import GridReport from "../../_SelComponents/_SelWebComponents/GridReport/GridReport";
 
 export default function PageTrack(props) {
-    if (props.loginData.user === null) {
-        return (
-            <Redirect to="/" />
-        )
-    }
+  if (props.loginData.user === null) {
+    return <Redirect to="/" />;
+  }
 
-    const LangElements = langJSON();
-    const lang = props.lang;
+  const LangElements = langJSON();
+  const lang = props.lang;
 
-    function lng(key) {
-        return Gl.LANG_GET_FormItem(LangElements, key, lang)
-    }
+  function lng(key) {
+    return Gl.LANG_GET_FormItem(LangElements, key, lang);
+  }
 
-    return (
-        <div className="page-track">
-            <HeaderLine
-                lang={props.lang}
-                selectedPage={"track"}
-                loginData={props.loginData}
-            />
+  return (
+    <div className="page-track">
+      <HeaderLine
+        lang={props.lang}
+        selectedPage={"track"}
+        loginData={props.loginData}
+      />
 
-            <div className="page-track-container">
-                <div className="column-1">
-                    <div className="UPS_search-panel">
-                        <div className="UPS_search-title-area">
-                            <h3>{lng("form-title")}</h3>
-                        </div>
-
-                        <div className="UPS_search-text-area">
-                            <p className="UPS_search-sub-title">Waybill No. / Plate No.:</p>
-                            <form className="UPS_search-form-area" name="YASHPTOP">
-                                <div className="UPS_search-input">
-                                    <input type="text" placeholder="UPS-formatted-No" name="T1" />
-                                </div>
-
-                                <div className="UPS_search-tracking-radio">
-                                    <ul>
-                                        <li>
-                                            <input id="UPS_input-radio-waybill" type="radio" name="tracking" value="hbl"
-                                                checked="checked" />
-                                            <label for="UPS_input-radio-waybill">Waybill No.</label>
-                                        </li>
-                                        <li>
-                                            <input id="UPS_input-radio-plateno" type="radio" name="tracking" value="cntr" />
-                                            <label for="UPS_input-radio-plateno">Plate No.</label>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div className="UPS_search-tracking-submit">
-                                    <button className="btn btn-success" id="UPS_search-tracking-submit" type="submit">###Search</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="column-2">
-                    <div>
-                        <img src={backgroundImage} alt="background_1.png" />
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-    )
+      <div className="page-track-container">
+        <GridReport
+          id="REPORT_TrackAndTrace"
+          lang={props.lang}
+          Filters={<TevaTrckAndTraceFilter />}
+          report={{
+            reportId: "REPORT_TEVA_TIG",
+            languageElements: langJSON(),
+            recordset: {
+              defaultSqlSelect: "ID, TIG_PositionNumber",
+              defaultSqlTop: 10,
+              sqlFrom: "U_UPS_TEVA_TIG_DATA_VW",
+              defaultSqlOrderBy: "TIG_PositionNumber",
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 }
---
+++ WEBSITE/upsreact/src/Pages/Track/subComponents/TevaTrackAndTraceFilter/TevaTrackAndTraceFilter.js
@@ -0,0 +1,51 @@
+export default function TevaTrackAndTraceFilter(props) {
+    return (
+        <div className="TrackAndTraceReportFilters">
+            <div className="row g-3 align-items-center">
+                <div className="col-2">
+                    <label className="col-form-label">Order Number:</label>
+                </div>
+                <div className="col-3">
+                    <input type="text" className="form-control reportFilter" data-sql="OrderNum like'?%'" />
+                </div>
+            </div>
+            <br />
+            <div className="row g-3 align-items-center">
+                <div className="col-2">
+                    <label className="col-form-label">Unit ID number:</label>
+                </div>
+                <div className="col-3">
+                    <input type="text" className="form-control reportFilter" data-sql="DocNum01 like'?%'" />
+                </div>
+            </div>
+            <br />
+            <div className="row g-3 align-items-center">
+                <div className="col-2">
+                    <label className="col-form-label">ETA:</label>
+                </div>
+                <div className="col-3">
+                    <input type="date" className="form-control reportFilter" data-sql="Loading_from >=?" />
+                </div>
+                <div className="col-3">
+                    <input type="date" className="form-control reportFilter" data-sql="Loading_from <=?(2359)" />
+                </div>
+            </div>
+            <br />
+            <div className="row g-3 align-items-center">
+                <div className="col-2">
+                    <label className="col-form-label">Temperature:</label>
+                </div>
+                <div className="col-3">
+                    <select className="form-select reportFilter" aria-label="Default select example" data-sql="GDS_UserFld_nvarchar03='?'">
+                        <option value=""></option>
+                        <option value="2-8">2-8</option>
+                        <option value="2-25">2-25</option>
+                        <option value="8-15">8-15</option>
+                        <option value="15-25">15-25</option>
+                        <option value="KH">KH</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    )
+}
